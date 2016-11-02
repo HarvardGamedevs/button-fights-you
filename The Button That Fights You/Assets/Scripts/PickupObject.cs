81dd9d035c1cdb83dc61f3606ecafb59fd5981d4
Chris Rodowicz <crodowicz@college.harvard.edu>
2016-11-01 23:58:15 -0400

    added grant pickupable objects

---
+++ The Button That Fights You/Assets/Scripts/PickupObject.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupObject : MonoBehaviour {
+	GameObject mainCamera;
+	bool carryin;
+	GameObject carriedObject;
+	public float distance;
+	public float smooth;
+	// Use this for initialization
+	void Start () {
+		mainCamera = GameObject.FindWithTag ("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (carryin)
+		{
+			carry(carriedObject);
+			checkdrop();
+		}
+		else
+		{
+			pickup();
+		}
+	}
+
+	void carry(GameObject c)
+	{
+		c.transform.position = Vector3.Lerp (c.transform.position, mainCamera.transform.position + mainCamera.transform.forward * distance, Time.deltaTime * smooth);
+	}
+	void checkdrop()
+	{
+		if (Input.GetKeyDown (KeyCode.E))
+		{
+			drop();
+		}
+	}
+
+	void drop ()
+	{
+		carryin = false;
+		carriedObject.GetComponent<Rigidbody>().isKinematic = false;
+		carriedObject = null;
+	}
+
+	void pickup()
+	{
+		if (Input.GetKeyDown(KeyCode.E))
+		{
+			int x = Screen.width / 2;
+			int y = Screen.height / 2;
+			Ray ray = mainCamera.GetComponent<Camera> ().ScreenPointToRay (new Vector3 (x, y));
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit)) {
+				Pickupable p = hit.collider.GetComponent<Pickupable>();
+				if (p != null) {
+					carryin = true;
+					carriedObject = p.gameObject;
+					carriedObject.GetComponent<Rigidbody>().isKinematic = true;
+				}
+			}
+		}
+	}
+
+}
